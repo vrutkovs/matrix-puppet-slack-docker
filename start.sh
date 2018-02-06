dfb71b6a632be1a76dbc05c667265d7e5fa8060f
isra17 <isra017@gmail.com>
2018-02-06 16:23:16 -0500

    Remove registration steps

--- start.sh
+++ start.sh
@@ -2,12 +2,4 @@ cd /app
 
 ln -s /data/config.json /app/config.json
 
-if [ ! -f /data/slack-registration.yaml ]; then
-  touch /tmp/creds
-  echo $MATRIX_LOCALPART >> /tmp/creds
-  echo $MATRIX_PASSWORD >> /tmp/creds
-  node index.js -r -u $APPSERVICE_URL < /tmp/creds
-  rm /tmp/creds
-fi
-
 exec node index.js
