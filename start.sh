5ba669088f3d817ec59050e4661d83f934be9784
isra17 <isra017@gmail.com>
2018-02-06 15:42:04 -0500

    Add credentials for registration

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /app
 ln -s /data/config.json /app/config.json
 
 if [ ! -f /data/slack-registration.yaml ]; then
-  node index.js -r -u $APPSERVICE_URL
+  echo -e "$MATRIX_LOCAPART\n$MATRIX_PASSWORD" |  node index.js -r -u $APPSERVICE_URL
 fi
 
 exec node index.js
